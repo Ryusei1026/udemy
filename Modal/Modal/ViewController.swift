2a7894da0590f4f0366409e3bf63ec2c567f2f86
Ryusei Takano <s1250034@gmail.com>
2020-06-06 23:54:56 +0900

    add Modal

---
+++ Modal/Modal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Modal
+//
+//  Created by 高野隆正 on 2020/06/05.
+//  Copyright © 2020 高野隆正. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
