fd9a7b971cd8dc574495761dbf10efc4775a1d6b
Ryusei Takano <s1250034@gmail.com>
2020-06-15 02:56:46 +0900

    add ChatApp

---
+++ ChatApp/ChatApp/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by 高野隆正 on 2020/06/15.
+//  Copyright © 2020 高野隆正. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
